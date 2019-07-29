a89fa925a13e4b3de8bebb5f977ade1dba1ac35f
Anisha-25 <53147663+Anisha-25@users.noreply.github.com>
2019-07-29 12:29:09 +0530

    Add files via upload

---
+++ Day2-Weekend.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+3-2
+
+
+# In[2]:
+
+
+0.1+0.2
+
+
+# In[3]:
+
+
+x=2
+y=3
+z=4
+x,y,z=2,3,4
+
+
+# In[4]:
+
+
+MAX_CONNECTIONS = 100
+
+
+# In[8]:
+
+
+bicycles = ["hero", 'redline', 'trek', 'ranger']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[2].title())
+print(bicycles[2].upper())
+
+
+# In[ ]:
+
+
+
+
